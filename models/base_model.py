01ea4c30674a1e9fd669abddc13f4cece837b6fc
Ainme <a.ndayishim@alustudent.com>
2023-10-06 14:47:03 +0200

    Some updates

--- models/base_model.py
+++ models/base_model.py
@@ -5,18 +5,18 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
+
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    # used for db storage
-    id = Column(String(60), nullable=False, primary_key=True)
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+        """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -54,26 +54,25 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
-    def delete(self):
-        """Deletes the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
-
     def to_dict(self):
         """Convert instance into dict format"""
-
         dictionary = {}
         dictionary.update(self.__dict__)
         for key in self.__dict__.keys():
             if key == "_sa_instance_state":
                 del (dictionary[key])
 
-        dictionary.update({'__class__': self.__class__.__name__})
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,3 +4,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Path: setup_mysql_dev.sql
+-- Compare this snippet from setup_mysql_test.sql:
